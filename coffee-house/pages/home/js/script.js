7c9f74e85c6e06a1c77aa230a24157185f4d8ae2
varenik40000 <64190191+varenik40000@users.noreply.github.com>
2023-12-11 13:58:46 +0500

    feat: add burger menu menu page

--- coffee-house/pages/home/js/script.js
+++ coffee-house/pages/home/js/script.js
@@ -1,22 +1,22 @@
 function showBurger(){
     let menu__icon = document.querySelector('.nav-menu__icon'),
     menuBody = document.querySelector('.nav-menu'),
+    menu__list = document.querySelector('.nav-menu__list'),
     header__link = document.querySelector('.header__link');
-
-    // console.log(menu__icon)
-    // console.log(menuBody)
     
     menu__icon.addEventListener('click', () => {
 
-        document.body.classList.toggle('_lock');
-        menu__icon.classList.toggle('_active');
-        menuBody.classList.toggle('_active');
-        header__link.classList.toggle('_active');
+        document.body.classList.add('_lock');
+        menu__icon.classList.add('_active');
+        menuBody.classList.add('_active');
+        header__link.classList.add('_active');
 
-        // console.log(menu__icon)
-        // console.log(menuBody)
-        // console.log(document.body)
-
+      })
+      menu__list.addEventListener('click', () => {
+        document.body.classList.remove('_lock');
+        menu__icon.classList.remove('_active');
+        menuBody.classList.remove('_active');
+        header__link.classList.remove('_active');
       })
 
 }
--- coffee-house/pages/menu/js/script.js
+++ coffee-house/pages/menu/js/script.js
@@ -1,4 +1,27 @@
-// alert('Hello');
+function showBurger(){
+    let menu__icon = document.querySelector('.nav-menu__icon'),
+    menuBody = document.querySelector('.nav-menu'),
+    menu__list = document.querySelector('.nav-menu__list'),
+    header__link = document.querySelector('.header__link');
+    
+    menu__icon.addEventListener('click', () => {
+
+        document.body.classList.add('_lock');
+        menu__icon.classList.add('_active');
+        menuBody.classList.add('_active');
+        header__link.classList.add('_active');
+
+      })
+      menu__list.addEventListener('click', () => {
+        document.body.classList.remove('_lock');
+        menu__icon.classList.remove('_active');
+        menuBody.classList.remove('_active');
+        header__link.classList.remove('_active');
+      })
+
+}
+showBurger()
+
 
 async function getProductJSON() {
     const res = await fetch(`../../pages/menu/products.json`);
